ffcd5cd9d9684434ecda4bb0dea87c511c670509
Ashukr321 <coderashukr321@gmail.com>
2026-04-15 20:31:34 +0530

    feat(string):string object to primitive string using valueOf methods

--- string-methods/_32_valueOf/valueOf.js
+++ string-methods/_32_valueOf/valueOf.js
@@ -11,3 +11,7 @@ console.log(stringObj.valueOf()); // "Hello"
 // Compare String object with its primitive value
 console.log(stringObj === "Hello"); // false (object vs primitive)
 console.log(stringObj.valueOf() === "Hello"); // true (primitive vs primitive)
+
+
+// we already seen string object to convert primitive string using toString methods : 
+// similarly we can do the same by using valueOf() methods 
