c444ef8bab94079937752f98e352a24743bbc300
Ashukr321 <coderashukr321@gmail.com>
2026-04-15 20:35:00 +0530

    feat(string):split methods use case

--- string-methods/_21_split/split.js
+++ string-methods/_21_split/split.js
@@ -19,3 +19,5 @@ const str3 = "";
 console.log(str3.split(""));// return empty array bec split method return a array of split based on seperator 
 
 
+// split method devide the string into substring and put all substring into array format
+// split("separator",limit): how many number we want to separate 
