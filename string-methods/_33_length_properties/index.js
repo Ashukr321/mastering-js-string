8bfefcf8d0834f674427965001a320d56d7dfb2e
Ashukr321 <coderashukr321@gmail.com>
2026-04-18 21:31:44 +0530

    feat(string):string methods

---
+++ string-methods/_33_length_properties/index.js
@@ -0,0 +1,23 @@
+let userName = "Ashutosh"; // 8 
+// console.log(userName.length); // 8 
+// // properties :   eg length
+// // methods () : bracket use in the methods  , toUppercase()
+ 
+// // indexing : index start from 0 , 
+// console.log(userName.at(0)); //'A'
+
+// console.log(userName.at(userName.length-2));
+
+
+// length✅
+// at() ✅
+// charAt(); ✅
+// toUppercase();✅
+// toLowercase();✅
+
+
+
+console.log(userName.at(2));
+
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
